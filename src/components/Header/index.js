bfd10f2d5118c03cc0368aa00d7cb55072423cab
Visvaldas <visva.rapalis@gmail.com>
2021-03-08 16:22:08 +0200

    Add logo and style it

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './styles.scss';
+import Logo from './../../assets/logo.png';
 
-const Header = props => {
+const Header = (props) => {
   return (
-    <header className="header">
-    
+    <header className='header'>
+      <div className='wrap'>
+        <div className='logo'>
+          <img src={Logo} alt='Logo' />
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
