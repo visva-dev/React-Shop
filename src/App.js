b346aca1c23bdebb1a6d69aec500d7d6086e72c4
Visvaldas <visva.rapalis@gmail.com>
2021-03-08 17:24:15 +0200

    Create HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './default.scss';
 import Header from './components/Header';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className='App'>
     <Header />
-    Hello!!!!dfgdfgdfgdfgdfgdfgdfgfdg
+    <Homepage />
     </div>
   );
 }
