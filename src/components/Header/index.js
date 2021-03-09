9ee9319ac9f92d98970c4f23431bbbe03a500e94
Visvaldas <visva.rapalis@gmail.com>
2021-03-09 15:02:20 +0200

    Implement reudx connect

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import './styles.scss';
 import { Link } from 'react-router-dom';
 import Logo from './../../assets/logo.png';
@@ -49,4 +50,8 @@ Header.defaultProps = {
   currentUser: null
 };
 
-export default Header;
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
+export default connect(mapStateToProps, null)(Header);
