65a606ef96ddd42552820c10a32126758735d7fb
Visvaldas <visva.rapalis@gmail.com>
2021-03-09 10:37:07 +0200

    Create Header component and style it

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './styles.scss';
 import { Link } from 'react-router-dom';
 import Logo from './../../assets/logo.png';
+import { auth } from './../../firebase/utils';
 
 const Header = (props) => {
+  const { currentUser } = props;
   return (
     <header className='header'>
       <div className='wrap'>
@@ -13,15 +15,31 @@ const Header = (props) => {
           </Link>
         </div>
         <div className='callToActions'>
-          <ul>
-            <li>
-              <Link to='/registration'>Register</Link>
-            </li>
-          </ul>
+          {currentUser && (
+            <ul>
+              <li>
+                <span onClick={() => auth.signOut()}>LogOut</span>
+              </li>
+            </ul>
+          )}
+          {!currentUser && (
+            <ul>
+              <li>
+                <Link to='/registration'>Register</Link>
+              </li>
+              <li>
+                <Link to='/login'>Login</Link>
+              </li>
+            </ul>
+          )}
         </div>
       </div>
     </header>
   );
+};
+
+Header.defaultProps = {
+  currentUser: null,
 };
 
 export default Header;
